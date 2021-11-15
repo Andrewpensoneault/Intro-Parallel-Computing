338ed15a3d1085635a1843389ff3c58cbea9d512
Andrew Pensoneault <andrew-pensoneault@uiowa.edu>
2021-11-15 14:16:56 -0600

    Update mpi_wait.c

--- mpi_wait.c
+++ mpi_wait.c
@@ -2,13 +2,13 @@
 #include <mpi.h>
 
 int main(int argc, char** argv){
-    int process_Rank, size_Of_Cluster;
+    int process_Rank, size_Of_Cluster, i;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
-    for(int i = 0, i < size_Of_Cluster, i++){
+    for(i = 0, i < size_Of_Cluster, i++){
         if(i == process_Rank){
             printf("Hello World from process %d of %d at flag 1\n", process_Rank, size_Of_Cluster);
         }
@@ -20,7 +20,7 @@ int main(int argc, char** argv){
         printf("All processes passed flag 1");
     }
     
-    for(int i = 0, i < size_Of_Cluster, i++){
+    for(i = 0, i < size_Of_Cluster, i++){
         if(i == process_Rank){
             printf("Hello World from process %d of %d at flag 2\n", process_Rank, size_Of_Cluster);
         }
