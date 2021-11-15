3b28bdf35d4c9c3e4045c6ec63e10ecb442006e0
Andrew Pensoneault <andrew-pensoneault@uiowa.edu>
2021-11-15 14:03:29 -0600

    Updated mpi program

--- mpi_hello_world.c
+++ mpi_hello_world.c
@@ -9,6 +9,6 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
     printf("Hello World from process %d of %d\n", process_Rank, size_Of_Cluster);
-
+    MPI_Finalize();
     return 0;
 }
