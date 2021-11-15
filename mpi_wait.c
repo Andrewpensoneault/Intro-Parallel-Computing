3258fee7a001cdd11a2679d7b491cad16ec98078
Andrew Pensoneault <andrew-pensoneault@uiowa.edu>
2021-11-15 14:14:41 -0600

    Added wait

---
+++ mpi_wait.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv){
+    int process_Rank, size_Of_Cluster;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
+
+    for(int i = 0, i < size_Of_Cluster, i++){
+        if(i == process_Rank){
+            printf("Hello World from process %d of %d at flag 1\n", process_Rank, size_Of_Cluster);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }    
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (process_Rank == 0){
+        printf("All processes passed flag 1");
+    }
+    
+    for(int i = 0, i < size_Of_Cluster, i++){
+        if(i == process_Rank){
+            printf("Hello World from process %d of %d at flag 2\n", process_Rank, size_Of_Cluster);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }    
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (process_Rank == 0){
+        printf("All processes passed flag 2");
+    }
+
+    MPI_Finalize();
+    return 0;
+}
